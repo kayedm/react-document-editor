5970c5a2906098af63439110f33a28d2305d20ac
Kayed <103891908+kayedm@users.noreply.github.com>
2026-07-05 17:46:45 -0400

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/react-document-editor",
-    assetPrefix: "/react-document-editor/",
-    images: {
-        unoptimized: true,
-    },
     eslint: {
         ignoreDuringBuilds: true,
     },
